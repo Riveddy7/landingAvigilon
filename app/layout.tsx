b042177ca433943d5abb3e0a08b3b98630391ed0
Riveddy7 <rivera.eduardo7@icloud.com>
2025-12-05 17:02:55 -0800

    Etiqueta Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { BitrixWidget } from "@/components/BitrixWidget";
 import { ThreeCXWidget } from "@/components/ThreeCXWidget";
@@ -27,6 +28,21 @@ export default function RootLayout({
         {children}
         <BitrixWidget />
         {/* <ThreeCXWidget /> */}
+
+        {/* Google Tag Manager */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2VZXNDKWQH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2VZXNDKWQH');
+          `}
+        </Script>
       </body>
     </html>
   );
